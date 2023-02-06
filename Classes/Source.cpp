b1bcfbcce88f13573413c15f4c20f469295bc696
Louis Doherty <louisdoherty98@gmail.com>
2023-02-06 17:17:19 +0000

    Changed variable name to be less misleading

--- Classes/Source.cpp
+++ Classes/Source.cpp
@@ -8,11 +8,11 @@ void automaticVsDynamic() {
 	scriptWriter.happyBirthday();
 	std::cout << std::endl;
 
-	Person* wrongUse = new Person(27, "Louis"); //dynamic allocation
-	wrongUse->happyBirthday();
-	std::cout << std::endl << wrongUse;
-	wrongUse = new Person(28, "Dave");
-	wrongUse->happyBirthday(); //arrows rather than dots
+	Person* riskyUse = new Person(27, "Louis"); //dynamic allocation
+	riskyUse->happyBirthday();
+	std::cout << std::endl << riskyUse;
+	riskyUse = new Person(28, "Dave");
+	riskyUse->happyBirthday(); //arrows rather than dots
 	std::cout << std::endl;
 }
 
