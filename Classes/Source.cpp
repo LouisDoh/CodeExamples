7941e3cf6422b4cfcafa22edc79beb13505804f4
Louis Doherty <louisdoherty98@gmail.com>
2023-02-06 17:06:50 +0000

    Moved all examples to their own function - to demo code, simply call it in main

--- Classes/Source.cpp
+++ Classes/Source.cpp
@@ -1,8 +1,9 @@
 #include "Person.h"
 #include "DumbData.h"
+#include "Dog.h"
 #include <iostream>
 
-int main() {
+void automaticVsDynamic() {
 	Person scriptWriter(24, "Louis"); //automatic/'default' allocation
 	scriptWriter.happyBirthday();
 	std::cout << std::endl;
@@ -13,11 +14,21 @@ int main() {
 	wrongUse = new Person(28, "Dave");
 	wrongUse->happyBirthday(); //arrows rather than dots
 	std::cout << std::endl;
+}
 
+void structEx() {
 	DumbData myStruct;
 	myStruct.x = 5;
 	myStruct.y = 5;
 	//myStruct.funFunction(); - inaccessible
 	std::cout << "X: " << myStruct.x << " Y: " << myStruct.y;
+}
 
+void inheritanceEx() {
+	Dog myDog(9, 40, "Fido", "Brown");
+	myDog.introduce();
+}
+
+int main() {
+	inheritanceEx();
 }
